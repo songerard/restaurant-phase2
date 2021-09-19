d2b15313aeaaafd3483c752d0b0cddb15efa6f42
songerard <songerard@gmail.com>
2021-09-19 22:44:13 +0800

    Add search function of index page

--- app.js
+++ app.js
@@ -28,6 +28,20 @@ app.get('/restaurants/:id', (req, res) => {
   res.render('show', { restaurant: restaurant })
 })
 
+// search restaurant
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim()
+  const restaurants = restaurantList.results.filter(r => r.name.toLowerCase().includes(req.query.keyword.toLowerCase()) || r.category.toLowerCase().includes(req.query.keyword.toLowerCase()))
+
+  if (!restaurants.length || !keyword) {
+    // if no restaurant found
+    res.render('index', { restaurant: restaurantList.results, keyword: keyword, searchAlert: true })
+  } else {
+    // if some restaurants found
+    res.render('index', { restaurant: restaurants, keyword: keyword, searchAlert: false })
+  }
+})
+
 // set listen to localhost:3000
 app.listen(port, () => {
   console.log(`Express is listening to http://localhost:${port}`)
